dc20e54b741b0bcdc908ec6aaf9625870b2f57a4
booban1 <simonnyongesa6@gmail.com>
2019-12-18 10:27:16 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById("foot01").innerHTML = "<p>&copy; " + new Date().getFullYear() + "Booban Design. All rights reserved </P>";
+document.getElementById("nav01").innerHTML = "<ul id='menu'>" + "<li><a href ='index.html'> Home</a></li>" + "<li><a href= 'customers.html'> Data </a></li>" + "<li><a href='about.html'> About</a></Li>" + "<li><a href='login.html'> Login </a></Li>" +  "</ul>";
